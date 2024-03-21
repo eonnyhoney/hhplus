ced2180c1819f73c703fe45e0b039d4cc38eccf8
eonnyhoney <eonnyhoney@gmail.com>
2024-03-21 22:13:23 +0900

    feat: 꺄악

--- packages/chapter2/src/render.js
+++ packages/chapter2/src/render.js
@@ -1,9 +1,8 @@
-export function jsx(type, props, ...children) {
-  return {type, props, children: children.flat()}
+export function jsx(type, props = {}, ...children) {
+  return {type, props, children}
 }
 
 export function createElement(node) {
-  // jsx를 dom으로 변환
   if (typeof node === 'string') {
     return document.createTextNode(node);
   } else {
@@ -18,73 +17,58 @@ export function createElement(node) {
   }
 }
 
-function updateAttributes(target, newProps = {}, oldProps = {}) {
-  // newProps들을 반복하여 각 속성과 값을 확인
-  //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
-  //     다음 속성으로 넘어감 (변경 불필요)
-  //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
-  //     target에 해당 속성을 새 값으로 설정
-  for (const [key, value] of Object.entries(newProps)) {
-    if (oldProps[key] === value) {
-      continue;
+function updateAttributes(target, newProps, oldProps) {
+  if (!newProps) {
+    for (const key in oldProps) {
+      target.removeAttribute(key);
     }
-
-    target.setAttribute(key, value);
-  }
-
-  // oldProps을 반복하여 각 속성 확인
-  //   만약 newProps들에 해당 속성이 존재한다면
-  //     다음 속성으로 넘어감 (속성 유지 필요)
-  //   만약 newProps들에 해당 속성이 존재하지 않는다면
-  //     target에서 해당 속성을 제거
-  for (const key in oldProps) {
-    if (newProps[key]) {
-      continue;
+  } else {
+    for (const [key, value] of Object.entries(newProps)) {
+      if (oldProps[key] !== value) {
+        target.setAttribute(key, value);
+      }
     }
-
-    target.removeAttribute(key);
+    for (const key in oldProps) {
+      if (!newProps[key]) {
+        target.removeAttribute(key);
+      }
+    }
   }
 }
 
 export function render(parent, newNode, oldNode = {}, index = 0) {
-  // 1. 만약 newNode가 없고 oldNode만 있다면
-  //   parent에서 oldNode를 제거
-  //   종료
   if (!newNode && oldNode) {
-    return parent.removeChild(parent.childNodes[index]);
-
+    parent.removeChild(parent.childNodes[index]);
+    return
   }
 
-  // 2. 만약 newNode가 있고 oldNode가 없다면
-  //   newNode를 생성하여 parent에 추가
-  //   종료
   if (newNode && !oldNode) {
-    return parent.appendChild(createElement(newNode));
-
+    parent.appendChild(createElement(newNode));
+    return
   }
 
-  // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
-  //   oldNode를 newNode로 교체
-  //   종료
   if (typeof newNode === 'string' && typeof oldNode === 'string' && newNode !== oldNode) {
-    return parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+    if (index < parent.childNodes.length) {
+      parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+    } else {
+      parent.appendChild(createElement(newNode));
+    }
+    return
   }
 
-  // 4. 만약 newNode와 oldNode의 타입이 다르다면
-  //   oldNode를 newNode로 교체
-  //   종료
   if (newNode.type !== oldNode.type) {
-    return parent.replaceChild(createElement(newNode), parent.childNodes[index]);
-
+    if (index < parent.childNodes.length) {
+      parent.replaceChild(createElement(newNode), parent.childNodes[index]);
+    } else {
+      parent.appendChild(createElement(newNode));
+    }
+    return
   }
 
-  // 5. newNode와 oldNode에 대해 updateAttributes 실행
-  updateAttributes(parent, newNode.props, oldNode.props);
+  updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
 
-  // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
-  //   각 자식노드에 대해 재귀적으로 render 함수 호출
-  const maxLength = Math.max(newNode.children.length, oldNode.children.length);
+  const maxLength = Math.max(newNode.children?.length, oldNode.children?.length);
   for (let i = 0; i < maxLength; i++) {
-    render(parent.childNodes(index), newNode.children[i], oldNode.children[i], i);
+    render(parent.childNodes[index], newNode.children[i], oldNode.children[i], i);
   }
 }
